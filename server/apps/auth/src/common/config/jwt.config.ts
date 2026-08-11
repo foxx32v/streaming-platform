6818e561b5df6c1b6276083a02ac4be1cc4477a2
1395262 <.com>
2026-08-11 22:31:28 +0300

    Im coming back. new configs

---
+++ server/apps/auth/src/common/config/jwt.config.ts
@@ -0,0 +1,11 @@
+require('dotenv').config({path: `config`})
+
+export const REFRESH_TOKEN = {
+    SECRET : process.env.REFRESH_SECRET,
+    EXPIRES : 15 * 1000 * 60 * 60 * 24, // day //
+}
+
+export const ACCESS_TOKEN = {
+    SECRET : process.env.ACCESS_SECRET,
+    EXPIRES : 5 * 1000 * 60 // min //
+}
---
+++ server/apps/auth/src/common/config/rateLimiter.config.ts
@@ -0,0 +1,17 @@
+export const GLOBAL_LIMITER = {
+    COUNT : 20,
+    TIMEOUT : 15 * 1000 * 60,
+    MESSAGE : 'Rate limit exceeded. Please try again later.'
+}
+
+export const REGISTER_LIMITER = {
+    COUNT : 3,
+    TIMEOUT : 60 * 1000 * 60,
+    MESSAGE : 'Registration limit exceeded. Please try again later.'
+}
+
+export const LOGIN_LIMITER = {
+    COUNT : 5,
+    TIMEOUT : 30 * 1000 * 60,
+    MESSAGE : 'Login limit exceeded. Please try again later.'
+}
